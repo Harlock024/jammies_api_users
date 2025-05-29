1ebef5f9a0db041b5a6f0e04bcc864facc6d5cf3
harlock024 <hadith024@gmail.com>
2025-05-29 04:41:44 -0600

    feat: playlist by user controller added

--- src/main/java/dev/jammies/jammies_api_users/playlist/PlaylistControllers.java
+++ src/main/java/dev/jammies/jammies_api_users/playlist/PlaylistControllers.java
@@ -1,77 +1,117 @@
 package dev.jammies.jammies_api_users.playlist;
 
-
 import dev.jammies.jammies_api_users.tracks.TrackResponse;
 import dev.jammies.jammies_api_users.users.User;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.UUID;
-
 @RestController
 @RequestMapping("api/playlist")
 public class PlaylistControllers {
-    private final PlaylistService playlistService;
 
+    private final PlaylistService playlistService;
 
     public PlaylistControllers(PlaylistService playlistService) {
         this.playlistService = playlistService;
     }
 
-    @GetMapping()
+    @GetMapping
     public ResponseEntity<List<PlaylistResponseDto>> getPlaylists() {
-        return new ResponseEntity<>(playlistService.getAllPlaylists(), HttpStatus.OK);
+        return new ResponseEntity<>(
+            playlistService.getAllPlaylists(),
+            HttpStatus.OK
+        );
     }
 
-    @PostMapping()
-    public ResponseEntity<PlaylistResponseDto> createPlaylist(@RequestBody
-                                                              CreatePlaylistRequestDto createPlaylistRequestDto,
-                                                              Authentication authentication
+    @GetMapping("/user")
+    public ResponseEntity<List<PlaylistResponseDto>> getUserPlaylists(
+        Authentication authentication
     ) {
         User user = (User) authentication.getPrincipal();
-        return new ResponseEntity<>(playlistService.createPlaylist(createPlaylistRequestDto.getName(), user), HttpStatus.OK);
+        return new ResponseEntity<>(
+            playlistService.getUserPlaylists(user.getId()),
+            HttpStatus.OK
+        );
+    }
+
+    @PostMapping
+    public ResponseEntity<PlaylistResponseDto> createPlaylist(
+        @RequestBody CreatePlaylistRequestDto createPlaylistRequestDto,
+        Authentication authentication
+    ) {
+        User user = (User) authentication.getPrincipal();
+        return new ResponseEntity<>(
+            playlistService.createPlaylist(
+                createPlaylistRequestDto.getName(),
+                user
+            ),
+            HttpStatus.OK
+        );
     }
 
     @PutMapping(path = "{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<PlaylistResponseDto> updatePlaylist(
-            @RequestPart PlaylistRequestDto playlistRequestDto,
-            @PathVariable UUID id,
-            Authentication authentication
+        @RequestPart PlaylistRequestDto playlistRequestDto,
+        @PathVariable UUID id,
+        Authentication authentication
     ) throws IOException {
         User user = (User) authentication.getPrincipal();
-        return new ResponseEntity<>(playlistService.updatePlaylist(id, playlistRequestDto), HttpStatus.OK);
+        return new ResponseEntity<>(
+            playlistService.updatePlaylist(id, playlistRequestDto),
+            HttpStatus.OK
+        );
     }
 
-    @DeleteMapping("{id}")
+    @DeleteMapping("{id}/")
     public ResponseEntity<Boolean> deletePlaylist(
-            @PathVariable UUID id,
-            Authentication authentication
+        @PathVariable UUID id,
+        Authentication authentication
     ) {
         User user = (User) authentication.getPrincipal();
-        return new ResponseEntity<>(playlistService.deletePlaylist(id), HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(
+            playlistService.deletePlaylist(id),
+            HttpStatus.NO_CONTENT
+        );
     }
 
     // tracks to playlist
 
     @PostMapping("{id}/tracks")
-    public ResponseEntity<PlaylistResponseDto> addTrackToPlaylist(@PathVariable UUID id, @RequestBody AddTrackRequestDto dto, Authentication authentication) {
+    public ResponseEntity<PlaylistResponseDto> addTrackToPlaylist(
+        @PathVariable UUID id,
+        @RequestBody AddTrackRequestDto dto,
+        Authentication authentication
+    ) {
         User user = (User) authentication.getPrincipal();
-        return new ResponseEntity<>(playlistService.AddTrackToPlaylist(id,dto.getTrack_id()), HttpStatus.OK);
+        return new ResponseEntity<>(
+            playlistService.AddTrackToPlaylist(id, dto.getTrack_id()),
+            HttpStatus.OK
+        );
     }
 
     @GetMapping("{id}/tracks")
-    public ResponseEntity<List<TrackResponse>> getTracksInPlaylist(@PathVariable UUID id, Authentication authentication) {
+    public ResponseEntity<List<TrackResponse>> getTracksInPlaylist(
+        @PathVariable UUID id,
+        Authentication authentication
+    ) {
         User user = (User) authentication.getPrincipal();
-        return new ResponseEntity<>(playlistService.getTracksInPlaylist(id), HttpStatus.OK);
+        return new ResponseEntity<>(
+            playlistService.getTracksInPlaylist(id),
+            HttpStatus.OK
+        );
     }
 
     @DeleteMapping("{id}/tracks/{trackId}")
-    public ResponseEntity<Void> removeTrackFromPlaylist(@PathVariable UUID id, @PathVariable UUID trackId) {
+    public ResponseEntity<Void> removeTrackFromPlaylist(
+        @PathVariable UUID id,
+        @PathVariable UUID trackId
+    ) {
         playlistService.removeTrackFromPlaylist(id, trackId);
         return ResponseEntity.noContent().build();
     }
--- src/main/java/dev/jammies/jammies_api_users/playlist/PlaylistService.java
+++ src/main/java/dev/jammies/jammies_api_users/playlist/PlaylistService.java
@@ -6,15 +6,14 @@ import dev.jammies.jammies_api_users.tracks.TrackResponse;
 import dev.jammies.jammies_api_users.tracks.TrackServices;
 import dev.jammies.jammies_api_users.users.User;
 import dev.jammies.jammies_api_users.utils.CloudinaryService;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 
 @Service
 public class PlaylistService {
@@ -25,7 +24,13 @@ public class PlaylistService {
     private final TrackServices trackServices;
     private final ContentNegotiatingViewResolver contentNegotiatingViewResolver;
 
-    public PlaylistService(PlaylistsRepository playlistsRepository, CloudinaryService cloudinaryService, TrackRepository trackRepository, TrackServices trackServices, ContentNegotiatingViewResolver contentNegotiatingViewResolver) {
+    public PlaylistService(
+        PlaylistsRepository playlistsRepository,
+        CloudinaryService cloudinaryService,
+        TrackRepository trackRepository,
+        TrackServices trackServices,
+        ContentNegotiatingViewResolver contentNegotiatingViewResolver
+    ) {
         this.playlistsRepository = playlistsRepository;
         this.cloudinaryService = cloudinaryService;
         this.trackRepository = trackRepository;
@@ -34,11 +39,27 @@ public class PlaylistService {
     }
 
     public List<PlaylistResponseDto> getAllPlaylists() {
-        return playlistsRepository.findAll().stream().map(this::convertToDto).collect(Collectors.toList());
+        return playlistsRepository
+            .findAll()
+            .stream()
+            .map(this::convertToDto)
+            .collect(Collectors.toList());
+    }
+
+    public List<PlaylistResponseDto> getUserPlaylists(User user) {
+        return playlistsRepository
+            .findAllByUser(user)
+            .stream()
+            .map(this::convertToDto)
+            .collect(Collectors.toList());
     }
 
     public PlaylistResponseDto getPlaylist(UUID playlistId) {
-        return playlistsRepository.findById(playlistId).map(this::convertToDto).orElseThrow(() -> new NoSuchElementException("Playlist not found"));
+        return playlistsRepository
+            .findById(playlistId)
+            .map(this::convertToDto)
+            .orElseThrow(() -> new NoSuchElementException("Playlist not found")
+            );
     }
 
     public PlaylistResponseDto createPlaylist(String name, User user) {
@@ -49,9 +70,14 @@ public class PlaylistService {
         return convertToDto(playlist);
     }
 
-    public PlaylistResponseDto updatePlaylist(UUID id, PlaylistRequestDto playlistRequestDto) throws IOException {
-
-        Playlist playlist = playlistsRepository.findById(id).orElseThrow(() -> new NoSuchElementException("Playlist not found"));
+    public PlaylistResponseDto updatePlaylist(
+        UUID id,
+        PlaylistRequestDto playlistRequestDto
+    ) throws IOException {
+        Playlist playlist = playlistsRepository
+            .findById(id)
+            .orElseThrow(() -> new NoSuchElementException("Playlist not found")
+            );
 
         if (playlistRequestDto.getName() != null) {
             playlist.setName(playlistRequestDto.getName());
@@ -62,7 +88,11 @@ public class PlaylistService {
         }
 
         if (playlistRequestDto.getCover() != null) {
-            var coverResult = cloudinaryService.upload(playlistRequestDto.getCover(), "jammies_track/playlist/cover", "image");
+            var coverResult = cloudinaryService.upload(
+                playlistRequestDto.getCover(),
+                "jammies_track/playlist/cover",
+                "image"
+            );
             if (coverResult == null) {
                 throw new IOException("Failed to upload cover");
             }
@@ -73,7 +103,6 @@ public class PlaylistService {
         return convertToDto(playlist);
     }
 
-
     public boolean deletePlaylist(UUID id) {
         if (!playlistsRepository.existsById(id)) {
             throw new NoSuchElementException("Playlist not found");
@@ -83,16 +112,31 @@ public class PlaylistService {
     }
 
     public PlaylistResponseDto convertToDto(@NotNull Playlist playlist) {
-        return new PlaylistResponseDto(playlist.getId(), playlist.getName(), playlist.getDescription(), playlist.getUser() != null ? playlist.getUser().getUsername() : null, playlist.getCover_url());
+        return new PlaylistResponseDto(
+            playlist.getId(),
+            playlist.getName(),
+            playlist.getDescription(),
+            playlist.getUser() != null
+                ? playlist.getUser().getUsername()
+                : null,
+            playlist.getCover_url()
+        );
     }
+
     // track to playlist
 
+    public PlaylistResponseDto AddTrackToPlaylist(
+        UUID playlistId,
+        UUID trackId
+    ) {
+        Playlist playlist = playlistsRepository
+            .findById(playlistId)
+            .orElseThrow(() -> new NoSuchElementException("Playlist not found")
+            );
 
-    public PlaylistResponseDto AddTrackToPlaylist(UUID playlistId, UUID trackId) {
-        Playlist playlist = playlistsRepository.findById(playlistId).orElseThrow(() -> new NoSuchElementException("Playlist not found"));
-
-        Track track = trackRepository.findById(trackId)
-                .orElseThrow(() -> new NoSuchElementException("Track not found"));
+        Track track = trackRepository
+            .findById(trackId)
+            .orElseThrow(() -> new NoSuchElementException("Track not found"));
 
         playlist.getTracks().add(track);
         playlistsRepository.save(playlist);
@@ -100,18 +144,27 @@ public class PlaylistService {
     }
 
     public List<TrackResponse> getTracksInPlaylist(UUID playlistId) {
-        Playlist playlist = playlistsRepository.findById(playlistId).orElseThrow(() -> new NoSuchElementException("Playlist not found"));
+        Playlist playlist = playlistsRepository
+            .findById(playlistId)
+            .orElseThrow(() -> new NoSuchElementException("Playlist not found")
+            );
 
-        return playlist.getTracks().stream()
-                .map(this::converTrackToDto)
-                .collect(Collectors.toList());
+        return playlist
+            .getTracks()
+            .stream()
+            .map(this::converTrackToDto)
+            .collect(Collectors.toList());
     }
 
     public Boolean removeTrackFromPlaylist(UUID playlistId, UUID trackId) {
-        Playlist playlist = playlistsRepository.findById(playlistId).orElseThrow(() -> new NoSuchElementException("Playlist not found"));
+        Playlist playlist = playlistsRepository
+            .findById(playlistId)
+            .orElseThrow(() -> new NoSuchElementException("Playlist not found")
+            );
 
-        Track Track = trackRepository.findById(trackId)
-                .orElseThrow(() -> new NoSuchElementException("Track not found"));
+        Track Track = trackRepository
+            .findById(trackId)
+            .orElseThrow(() -> new NoSuchElementException("Track not found"));
 
         playlist.getTracks().remove(Track);
         playlistsRepository.save(playlist);
@@ -121,13 +174,12 @@ public class PlaylistService {
 
     public TrackResponse converTrackToDto(Track track) {
         return new TrackResponse(
-                track.getId(),
-                track.getTitle(),
-                track.getDuration(),
-                track.getUser().getUsername(),
-                track.getTitle(),
-                track.getCover_image()
+            track.getId(),
+            track.getTitle(),
+            track.getDuration(),
+            track.getUser().getUsername(),
+            track.getTitle(),
+            track.getCover_image()
         );
     }
 }
-
